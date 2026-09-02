b0056ae8de7e9ad645f005d039fd181cdb109ce8
hardikkumar25-beep <hardikkumar.work25@gmail.com>
2026-09-02 11:54:24 +0000

    Sync LeetCode submission - Valid Sudoku (python)

---
+++ problems/valid_sudoku/solution.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        rows=[set() for _ in range(9)]
+        cols=[set() for _ in range(9)]
+        boxes=[set() for _ in range(9)]
+        for r in range(9):
+            for c in range(9):
+                num=board[r][c]
+                if num == '.':
+                    continue
+                if num in rows[r]:
+                    return False
+                rows[r].add(num)
+                if num in cols[c]:
+                    return False
+                cols[c].add(num)
+                box=(r//3)*3+c//3
+                if num in boxes[box]:
+                    return False
+                boxes[box].add(num)
+        return True
