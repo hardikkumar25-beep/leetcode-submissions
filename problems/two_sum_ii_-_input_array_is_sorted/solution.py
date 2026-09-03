7907b14c14d1e1d6628295a62aecd365503ff9e7
hardikkumar25-beep <hardikkumar.work25@gmail.com>
2026-09-03 12:05:04 +0000

    Sync LeetCode submission - Two Sum II - Input Array Is Sorted (python)

---
+++ problems/two_sum_ii_-_input_array_is_sorted/solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        l=0
+        r=len(numbers)-1
+        while l<r:
+            total=numbers[l]+numbers[r]
+            if total==target:
+                return [l+1,r+1]
+            elif total<target:
+                l+=1
+            else:
+                r-=1
+            
+        
