19277f98c1dfe311f869c98d8739211fc4a3224b
hardikkumar25-beep <hardikkumar.work25@gmail.com>
2026-09-23 17:05:11 +0000

    Sync LeetCode submission - Sliding Window Maximum (python)

---
+++ problems/sliding_window_maximum/solution.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        left=0
+        dq=deque()
+        result=[]
+        for right in range(len(nums)):
+            while dq and nums[dq[-1]]<nums[right]:
+                dq.pop()
+            dq.append(right)
+            if dq[0]<left:
+                dq.popleft()
+            if right-left+1==k:
+                result.append(nums[dq[0]])
+                left+=1
+        return result
+                    
